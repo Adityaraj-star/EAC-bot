226b7c9dd841d49c90777158a4e041568cce278c
Aditya Raj <adityaraj93107@gmail.com>
2025-06-30 16:41:49 +0530

    Made send button work by displaying user message

--- static/script.js
+++ static/script.js
@@ -1,1 +1,12 @@
-console.log("Script loaded.");
+function sendToBot() {
+    let inputBox = document.getElementById("msg-box");
+    let messageText = inputBox.value.trim();
+    let messageArea = document.getElementById("message-area");
+
+    if (messageText === "") return;
+
+    let userDiv = document.createElement("div");
+    userDiv.textContent = "You: " + messageText;
+    messageArea.appendChild(userDiv);
+    inputBox.value = "";
+}
